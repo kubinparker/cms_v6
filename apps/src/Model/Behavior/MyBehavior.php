4da8dc7077c5ea8889082910d9e30ce7f53ae89d
Nguyen Thanh Huy <huy.nguyenthanh@caters.co.jp>
2022-07-13 15:54:03 +0900

    modify

--- apps/src/Model/Behavior/MyBehavior.php
+++ apps/src/Model/Behavior/MyBehavior.php
@@ -210,7 +210,7 @@ class MyBehavior extends Behavior
 
     protected function buildForm($listPath)
     {
-        foreach ($listPath as $path) file_put_contents(APP . 'Form/' . ucfirst($this->slug) . 'Form.php', str_replace(['&=', '=&'], ['{', '}'], __(file_get_contents($path, true), ucfirst($this->slug))));
+        foreach ($listPath as $path) file_put_contents(APP . 'Form/' . ucfirst($this->slug) . 'Form.php', str_replace(['&=', '=&'], ['{', '}'], __(file_get_contents($path, true), [ucfirst($this->slug), $this->slug])));
     }
 
 
